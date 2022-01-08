ccf995049c7132981049608b6e94744ad005704b
jyryuitpro <jy.ryu.itpro@gmail.com>
2022-01-08 19:23:34 +0900

    원시 HTML 데이터 바인딩

--- src/router/index.js
+++ src/router/index.js
@@ -35,11 +35,13 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ '../views/Basic.vue')
   },
   {
+    path: '/databindinghtml',
+    name: 'DataBindingHtml',
+    component: () => import(/* webpackChunkName: "about" */ '../views/DataBindingHtml.vue')
+  },
+  {
     path: '/databinding',
     name: 'DataBinding',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/DataBinding.vue')
   },
 ]
---
+++ src/views/DataBindingHtml.vue
@@ -0,0 +1,34 @@
+<template>
+  <div v-html="htmlString"></div>
+</template>
+
+<script>
+export default {
+  name: "DataBindingHtml",
+  components: {},
+  data() {
+    return {
+      htmlString: '<p style="color: red;">This is a red string.</p>'
+    };
+  },
+  setup() {
+
+  },
+  created() {
+    //Vue Component가 생성이되는 순간에 실행
+  },
+  mounted() {
+    //Html이 실제 화면에 로딩이 되는 순간에 실행
+  },
+  unmounted() {
+    //다른 Component로 이동할 때, 실행
+  },
+  methods() {
+    //Component안에서 사용할 메소드(함수) 정의
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
