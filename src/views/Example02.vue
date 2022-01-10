ab55d36a1bb6ad6f16114a44e0dc297703ce6222
jyryuitpro <jy.ryu.itpro@gmail.com>
2022-01-11 00:00:23 +0900

    입력폼 데이터 바인딩 - select, checkbox, radio (복습) - select 데이터 바인딩 - checkbox 데이터 바인딩 - radio 데이터 바인딩

--- src/views/Example02.vue
+++ src/views/Example02.vue
@@ -1,30 +1,35 @@
 <template>
   <div>
+    <!-- select에서의 v-model은 value와 연결이 된다. -->
     <select v-model="city">
       <option value="02">서울</option>
       <option value="21">부산</option>
       <option value="064">제주</option>
     </select>
     <br>
+<!-- checkbox에서의 v-model은 checked와 연결이 된다. -->
 <!--    <label><input type="checkbox" v-model="checked">{{checked}}</label>-->
 <!--    <br>-->
-<!--    <label><input type="checkbox" value="서울" v-model="checked">서울</label>-->
-<!--    <label><input type="checkbox" value="부산" v-model="checked">부산</label>-->
-<!--    <label><input type="checkbox" value="제주" v-model="checked">제주</label>-->
-<!--    <br>-->
-<!--    <span>체크한 지역: {{checked}}</span>-->
-<!--    <br>-->
-<!--    <label><input type="radio" value="서울" v-model="picked"/>서울</label>-->
-<!--    <label><input type="radio" value="부산" v-model="picked"/>부산</label>-->
-<!--    <label><input type="radio" value="제주" v-model="picked"/>제주</label>-->
-<!--    <br>-->
-<!--    <span>선택한 지역: {{picked}}</span>-->
-<!--    <label><input type="radio" v-bind:value="radio1" v-model="picked"/>서울</label>-->
-<!--    <label><input type="radio" v-bind:value="radio2" v-model="picked"/>부산</label>-->
-<!--    <label><input type="radio" v-bind:value="radio3" v-model="picked"/>제주</label>-->
-<!--    <br>-->
-<!--    <span>선택한 지역: {{picked}}</span>-->
-<!--    <br>-->
+    <label><input type="checkbox" value="서울" v-model="checked">서울</label>
+    <label><input type="checkbox" value="부산" v-model="checked">부산</label>
+    <label><input type="checkbox" value="제주" v-model="checked">제주</label>
+    <br>
+    <span>체크한 지역: {{checked}}</span>
+    <br>
+<!-- radio v-model은 checked와 연결이 된다. -->
+    <label><input type="radio" value="서울" v-model="picked"/>서울</label>
+    <label><input type="radio" value="부산" v-model="picked"/>부산</label>
+    <label><input type="radio" value="제주" v-model="picked"/>제주</label>
+    <br>
+    <span>선택한 지역: {{picked}}</span>
+    <br>
+    <label><input type="radio" v-bind:value="radio1" v-model="picked"/>서울</label>
+    <label><input type="radio" v-bind:value="radio2" v-model="picked"/>부산</label>
+    <label><input type="radio" v-bind:value="radio3" v-model="picked"/>제주</label>
+    <br>
+    <span>선택한 지역: {{picked}}</span>
+    <br>
+    <!-- value에 데이터 바인딩하고 싶을 때는 속성 앞에 v-bind:를 넣어준다. -->
     <label><input type="radio" v-bind:value="radio[0]" v-model="picked"/>서울</label>
     <label><input type="radio" v-bind:value="radio[1]" v-model="picked"/>부산</label>
     <label><input type="radio" v-bind:value="radio[2]" v-model="picked"/>제주</label>
@@ -40,13 +45,17 @@ export default {
   data() {
     return {
       city: "21",
+
       // checked: false,
       checked: [],
-      radio: ["서울", "부산", "제주"],
+
+      picked: "",
+
       radio1: "서울2",
       radio2: "부산2",
       radio3: "제주2",
-      picked: "",
+
+      radio: ["서울", "부산", "제주"],
     };
   },
   setup() {
