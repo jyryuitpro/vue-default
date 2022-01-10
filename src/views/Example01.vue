18e5e0afea27254c9be6ea94b363ef682b89e8bc
jyryuitpro <jy.ryu.itpro@gmail.com>
2022-01-10 23:09:33 +0900

    입력폼 데이터 바인딩 - input text, number, textarea (복습) - Input type=text 데이터 바인딩 - Input type=number 데이터 바인딩 - textarea 데이터 바인딩

--- src/views/Example01.vue
+++ src/views/Example01.vue
@@ -1,9 +1,13 @@
 <template>
   <div>
-    <input type="text" v-model="valueModel"/>
-    <input type="number" v-model.number="valueModel2"/>
+    <!-- v-model == value -->
+    <input type="text" v-model="valueModel"/><!-- 문자열로 인식 -->
+    <br>
+    <input type="number" v-model.number="valueModel2"/><!-- 숫자로 인식 -->
+    <br>
     <textarea v-model="valueModel3"></textarea>
-    <div>{{valueModel4}}</div>
+    <br>
+    <div>{{valueModel3}}</div>
   </div>
 </template>
 
@@ -26,15 +30,18 @@ export default {
   },
   mounted() {
     //Html이 실제 화면에 로딩이 되는 순간에 실행
+    //data() {}에서 정의한 변수는 this.으로만 접근이 가능하다.
+    console.log(this.valueModel2);
+
     let valueModel4 = 11;
-    console.log(this.valueModel2)
-    console.log(valueModel4)
+    console.log(valueModel4);
   },
   unmounted() {
     //다른 Component로 이동할 때, 실행
   },
   methods() {
     //Component안에서 사용할 메소드(함수) 정의
+    //this.으로만 접근이 가능하다.
   },
 }
 </script>
