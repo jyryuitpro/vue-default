4f62e7b619efa4f41bd23c67cee092e7955528c5
jyryuitpro <jy.ryu.itpro@gmail.com>
2022-01-16 16:41:42 +0900

    Watch

--- src/views/Example09.vue
+++ src/views/Example09.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <!-- v-model.number 숫자로 인식 가능하도록 만들어줌 -->
     <input type="text" v-model.number="x1" />
     <input type="text" v-model.number="x2" />
 <!--    <button type="button" @click="plusNumber">계산</button>-->
@@ -21,6 +22,7 @@ export default {
   //data() {} 필드에 정의된 것만 사용할 수 있다.
   //데이터의 변경사항을 바로 감시하고, 내가 원하는 데이터 필드에 바로 그 값 갱신할 수 있다.
   //computed로 변환 가능
+  //해당 데이터 필드의 변경사항을 직접 감시
   watch: {
     x1() {
       this.y = this.x1 + this.x2;
@@ -29,6 +31,8 @@ export default {
       this.y = this.x1 + this.x2;
     },
   },
+  //data() {} 필드에 새로운 데이터를 선언하는 것과 유사하다. (데이터 필드를 만들어 주는 것)
+  //새로 선언된 데이터 필드에 사용된 데이터 필드의 변경사항을 감시하면서 새로 선언된 데이터 필드를 갱신 함
   computed: {
     // y() {
     //   return this.x1 + this.x2;
